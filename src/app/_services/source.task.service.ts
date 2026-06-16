5e8c417bff0919bd19cf9f04003546c1b3515a1f
Nabeel Ahmed <nabeel.amd93@gmail.com>
2026-06-16 09:17:15 -0500

    remove extract code

--- src/app/_services/source.task.service.ts
+++ src/app/_services/source.task.service.ts
@@ -55,10 +55,6 @@ export class SourceTaskService {
         return this.http.post<ApiResponse>(`${config.apiUrl}/sourceTask.json/listSourceTask`, this.searchText, { params: params });
     }
 
-    public fetchAllLinkSourceTaskWithSourceTaskTypeId(sourceTaskTypeId: any): Observable<ApiResponse> {
-        return this.http.get<ApiResponse>(`${config.apiUrl}/sourceTask.json/fetchAllLinkSourceTaskWithSourceTaskTypeId?sourceTaskTypeId=`+sourceTaskTypeId);
-    }
-
     public downloadListSourceTask(): Observable<any> {
         return this.http.get(`${config.apiUrl}/sourceTask.json/downloadListSourceTask`, {
             responseType: 'blob'
@@ -75,35 +71,6 @@ export class SourceTaskService {
         const formData = new FormData();
         formData.append("file", fileToUpload);
         return this.http.post(`${config.apiUrl}/sourceTask.json/uploadSourceTask`, formData);
-    }
-
-    public fetchAllLinkJobsWithSourceTaskId(payload:QueryCriteria): Observable<ApiResponse> {
-        if (payload.searchText) {
-            this.searchText = payload.searchText;
-        }
-        let params = new HttpParams();
-        if (payload.taskDetailId) {
-            params = params.append('sourceTaskId', payload.taskDetailId);
-        }
-        if (payload.startDate) {
-            params = params.append('startDate', payload.startDate);
-        }
-        if (payload.endDate) {
-            params = params.append('endDate', payload.endDate);
-        }
-        if (payload.page) {
-            params = params.append('page', payload.page);
-        }
-        if (payload.limit) {
-            params = params.append('limit', payload.limit);
-        }
-        if (payload.columnName) {
-            params = params.append('columnName', payload.columnName);
-        }
-        if (payload.order) {
-            params = params.append('order', payload.order);
-        }
-        return this.http.post<ApiResponse>(`${config.apiUrl}/sourceTask.json/fetchAllLinkJobsWithSourceTaskId`, this.searchText, { params: params });
     }
 
     public fetchSourceTaskWithSourceTaskId(payload:any): Observable<ApiResponse> {
